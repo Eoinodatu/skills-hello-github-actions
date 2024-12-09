6bc486920192b1251c519261f35925d3bb69c775
Eoinodatu <118973381+Eoinodatu@users.noreply.github.com>
2024-12-09 15:51:45 +0000

    Update feature43.java

--- feature43.java
+++ feature43.java
@@ -4,7 +4,8 @@
 public class KiltonHalloweenFeature {
 
     public static void main(String[] args) {
-        // Display a simple message for Kilton's Halloween event
+        // Method: displayWelcomeMessage
+        // Displays a welcome message for Kilton's Halloween shop.
         System.out.println("Welcome to Kilton's Halloween Shop!");
         System.out.println("Exclusive Halloween items are available for a limited time!");
         System.out.println("You can purchase items with credits or barter rare items.");
